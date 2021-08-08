2485d4a36c24d180e5c8e23ece16c7e902ff4d9a
Tobias Kunze <r@rixx.de>
2021-08-09 01:33:03 +0200

    Improve data retrieval with fallbacks

--- scripts/data.py
+++ scripts/data.py
@@ -47,15 +47,16 @@ def get_location_data(address=True, metadata=True):
 
     if address:
         try:
-            address_url = "https://nominatim.openstreetmap.org/lookup?osm_ids=N6107050239&format=json"
+            address_url = "https://nominatim.openstreetmap.org/lookup"
             result = request(
                 "get",
                 address_url,
                 params={
-                    "osm_ids": osm_place["osm_type"][0].upper() + osm_place["osm_id"],
+                    "osm_ids": osm_place["osm_type"][0].upper()
+                    + str(osm_place["osm_id"]),
                     "format": "json",
                 },
-            )[0]
+            )[0]["address"]
             location["address"] = {
                 "street": f"{result.get('road', '')} {result.get('house_number', '')}".strip(),
                 "place": result.get("town", ""),
@@ -71,7 +72,10 @@ def get_location_data(address=True, metadata=True):
         location["urls"] = {}
         location["description"] = ""
         location["external"] = {
-            "osm": {"type": osm_place["osm_type"][0].upper(), "id": osm_place["osm_id"]}
+            "osm": {
+                "type": osm_place["osm_type"][0].upper(),
+                "id": str(osm_place["osm_id"]),
+            }
         }
         try:
             details_url = "https://nominatim.openstreetmap.org/details.php"
@@ -80,7 +84,7 @@ def get_location_data(address=True, metadata=True):
                 details_url,
                 params={
                     "osmtype": osm_place["osm_type"][0].upper(),
-                    "osmid": osm_place["osm_id"],
+                    "osmid": str(osm_place["osm_id"]),
                     "addressdetails": 1,
                     "format": "json",
                 },
@@ -90,11 +94,40 @@ def get_location_data(address=True, metadata=True):
                 location["urls"]["home"] = home
             wikidata_id = result.get("extratags", {}).get("wikidata")
 
+            if not wikidata_id:
+                from .travel import get_yesno
+
+                add_id = get_yesno(
+                    "No Wikidata entry found. Do you want to enter a Wikipedia link?"
+                )
+                if add_id:
+                    wikidata_id = get_wikidata_id_from_url(
+                        inquirer.text("Wikipedia page")
+                    )
             if wikidata_id:
                 add_wikidata_information(location, wikidata_id)
         except Exception:
             pass
     return location
+
+
+def get_wikidata_id_from_url(url):
+    url = url.strip()
+    query_url = "https://www.wikidata.org/w/api.php"
+    title = url.split("/")[-1]
+    wiki = url.split(".")[0][-2:]  # assuming two-letter country codes
+    result = request(
+        "get",
+        query_url,
+        params={
+            "action": "wbgetentities",
+            "sites": f"{wiki}wiki",
+            "titles": title,
+            "format": "json",
+        },
+    )
+    if result.get("entities"):
+        return list(result["entities"].keys())[0]
 
 
 def add_wikidata_information(location, wikidata_id):
@@ -104,9 +137,13 @@ def add_wikidata_information(location, wikidata_id):
     result = list(request("get", wikidata_url)["entities"].values())[0]
 
     location["external"]["wikidata_id"] = wikidata_id
-    description_de = result.get("descriptions", {}).get("de", "")
-    description_en = result.get("descriptions", {}).get("en", "")
-    location["description"] = description_de + " / " + description_en
+    description_de = result.get("descriptions", {}).get("de", {}).get("value")
+    description_en = result.get("descriptions", {}).get("en", {}).get("value")
+
+    if description_de and description_en:
+        location["description"] = description_de + " / " + description_en
+    else:
+        location["description"] = description_de or description_en
 
     location["urls"]["wikipedia"] = result["sitelinks"].get("dewiki", {}).get(
         "url"
@@ -116,10 +153,41 @@ def add_wikidata_information(location, wikidata_id):
     )
 
     images = result.get("claims", {}).get("P18")
+    image = None
     if images:
         image = images[0]
         image_filename = image["mainsnak"]["datavalue"]["value"]
+    elif location["urls"]["wikipedia"]:
+        base_url = location["urls"]["wikipedia"][:25]
+        title = location["urls"]["wikipedia"].split("/")[-1]
+        media_query_url = f"{base_url}w/api.php"
+        result = request(
+            "get",
+            media_query_url,
+            params={
+                "action": "query",
+                "prop": "images",
+                "titles": title,
+                "format": "json",
+            },
+        )
+        try:
+            images = [
+                i["title"].replace(" ", "_")
+                for i in list(result["query"]["pages"].values())[0]["images"]
+                if i.get("title") and "commons-logo" not in i.get("title", "").lower()
+            ]
+            if images:
+                image_filename = inquirer.list_input(
+                    "Please choose an image",
+                    choices=[(f"{base_url}wiki/{image}", image) for image in images]
+                    + [("none of the above", None)],
+                )
+                image_filename = image_filename.split(":", maxsplit=1)[-1]
+        except Exception:
+            pass
 
+    if image_filename:
         image_md5 = hashlib.md5(image_filename.encode()).hexdigest()
         location[
             "cover_image_url"
@@ -141,15 +209,17 @@ def add_wikidata_information(location, wikidata_id):
         # We try and locate intended attribution, and if we find none, we fall back to user name and date
         author = result["user"]
         year = result["timestamp"][:4]
+        attribution_options = []
         if "Credit" in result["extmetadata"]:
-            attribution = result["extmetadata"]["Credit"]["value"]
-        elif "Artist" in result["extmetadata"]:
-            attribution = result["extmetadata"]["Artist"]["value"]
-        else:
-            attribution = f'<a href="{result["descriptionurl"]}">{author} ({year})'
+            attribution_options.append(result["extmetadata"]["Credit"]["value"])
+        if "Artist" in result["extmetadata"]:
+            attribution_options.append(result["extmetadata"]["Artist"]["value"])
+        attribution_options.append(
+            f'<a href="{result["descriptionurl"]}">{author} ({year})</a>'
+        )
 
         click.echo(
-            f"Please take care to look at the license at f{result['descriptionurl']} to make sure you're allowed to use it!"
+            f"Please take care to look at the license at {result['descriptionurl']} to make sure you're allowed to use it!"
         )
         choice = inquirer.list_input(
             message="Use this image?",
@@ -157,18 +227,17 @@ def add_wikidata_information(location, wikidata_id):
         )
 
         if choice is True:
-            location["cover_image_attribution"] = inquirer.text_input(
-                prompt="Image attribution", default=attribution
+            location["cover_image_attribution"] = inquirer.list_input(
+                message="Image attribution", choices=attribution_options
             )
         elif choice is False:
-            location["cover_image_url"] = inquirer.text_input(
-                prompt="Image URL", default=""
-            )
-            location["cover_image_attribution"] = inquirer.text_input(
-                prompt="Image attribution", default=""
+            location["cover_image_url"] = inquirer.text(message="Image URL", default="")
+            location["cover_image_attribution"] = inquirer.text(
+                message="Image attribution", default=""
             )
         else:
             location["cover_image_url"] = None
+            location["cover_image_attribution"] = None
 
 
 def get_komoot_route(start, end):
